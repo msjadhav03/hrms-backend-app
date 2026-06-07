3e1c08aa3a6ba8ca4841e62f7cc650dfba8b78a3
Manisha Jadhav <manishajadhav2323@gmail.com>
2026-06-07 09:01:52 +0530

    Cross Origin Resource access sharing enabled - For now for local

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.enableCors({
+    //  Specify allowed origins - For now updating local string
+    origin: configService.get<string>('ALLOWED_ORIGINS'),
+  });
+
   await app.listen(configService.get<number>('PORT') || 3000);
 }
 bootstrap();

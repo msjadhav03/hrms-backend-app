17f447625003ae9adf852da30ccbf8ab30ccb5a1
Manisha Jadhav <manishajadhav2323@gmail.com>
2026-06-09 14:46:52 +0530

    Created Application User  Schema

---
+++ src/database/schema/user.schema.ts
@@ -0,0 +1,14 @@
+export const usersTableCreation = `CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL,
+    employee_id VARCHAR(20) UNIQUE NOT NULL,
+    is_deleted BOOLEAN DEFAULT FALSE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_users
+    FOREIGN KEY(employee_id)
+    REFERENCES employees(employee_code)
+    ON DELETE CASCADE
+);`;

821f4c2a51684c6ef3c1cd324620c46e7e600737
Manisha Jadhav <manishajadhav2323@gmail.com>
2026-06-09 18:20:56 +0530

    Unit Testcases  - seeding.controller.ts

---
+++ src/seeding/seeding.controller.spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { SeedingController } from './seeding.controller';
+import { SeedingService } from './seeding.service';
+
+describe('SeedingController', () => {
+  let controller: SeedingController;
+  let mockSeedingService: Partial<SeedingService>;
+
+  beforeEach(async () => {
+    mockSeedingService = {
+      seedUsers: jest.fn(),
+    };
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [SeedingController],
+      providers: [
+        {
+          provide: SeedingService,
+          useValue: mockSeedingService,
+        },
+      ],
+    }).compile();
+
+    controller = module.get<SeedingController>(SeedingController);
+  });
+
+  it('Seeding should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});

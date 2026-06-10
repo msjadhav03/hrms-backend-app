a0607433e4139a2d383ad9dc6f697cbc3643eee2
Manisha Jadhav <manishajadhav2323@gmail.com>
2026-06-10 18:58:21 +0530

    Added Create database provider function to create tables

--- src/seeding/seeding.service.ts
+++ src/seeding/seeding.service.ts
@@ -1,7 +1,35 @@
-import { Injectable, Inject } from '@nestjs/common';
+import {
+  Injectable,
+  Inject,
+  InternalServerErrorException,
+  Logger,
+  HttpStatus,
+} from '@nestjs/common';
 import { Pool } from 'pg';
+import { CreateEmployeeSchema } from '../database/schema/employee.schema';
+import { UserEmployeeSchema } from '../database/schema/user.schema';
+import { SuccessMessages } from '../common/constants/messages';
 
 @Injectable()
 export class SeedingService {
   constructor(@Inject('PG_CONNECTION') private readonly pool: Pool) {}
+
+  private readonly logger = new Logger(SeedingService.name);
+  async createDatabase() {
+    try {
+      const result = await Promise.all([
+        this.pool.query(CreateEmployeeSchema),
+        this.pool.query(UserEmployeeSchema),
+      ]);
+      return {
+        status: HttpStatus.OK,
+        message: SuccessMessages.TABLE_CREATION_SUCCESS,
+      };
+    } catch (error) {
+      this.logger.error(
+        `Seeding Service: Runtime error occured ${error.message}`,
+      );
+      throw InternalServerErrorException;
+    }
+  }
 }

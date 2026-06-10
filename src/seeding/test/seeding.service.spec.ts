11e9c0d587a17ca2a4ecded69593b4a2268821ba
Manisha Jadhav <manishajadhav2323@gmail.com>
2026-06-10 19:17:04 +0530

    Added test cases for drop databse provider function

--- src/seeding/test/seeding.service.spec.ts
+++ src/seeding/test/seeding.service.spec.ts
@@ -56,10 +56,36 @@ describe('SeedingService', () => {
       });
     });
 
+    describe('dropDatabase', () => {
+      it(`should execute drop table queries and return a success message`, async () => {
+        (mockPool.query as jest.Mock).mockResolvedValue({ rows: [] });
+
+        const result = await service.dropDatabase();
+
+        expect(mockPool.query).toHaveBeenCalledTimes(2);
+        expect(mockPool.query).toHaveBeenNthCalledWith(
+          1,
+          `Drop table if exists employee cascade`,
+        );
+        expect(mockPool.query).toHaveBeenNthCalledWith(
+          2,
+          `Drop table if exists user cascade`,
+        );
+      });
+    });
+
     it('should log an error and throw InternalServerErrorException when a query fails', async () => {
       const mockError = new Error('Database connection failed');
       (mockPool.query as jest.Mock).mockRejectedValue(mockError);
       await expect(service.createDatabase()).rejects.toBe(
+        InternalServerErrorException,
+      );
+    });
+
+    it(`should log an error and throw InternalServerErrorExeption when a drop table queries failed`, async () => {
+      const mockError = new Error('Database connection failes');
+      (mockPool.query as jest.Mock).mockRejectedValue(mockError);
+      await expect(service.dropDatabase()).rejects.toBe(
         InternalServerErrorException,
       );
     });

9c5cb7b8da8850037f3a22785445d616804a8afe
Manisha Jadhav <manishajadhav2323@gmail.com>
2026-06-10 18:54:11 +0530

    Updated file name from constants.ts to messages.ts

---
+++ src/common/constants/messages.ts
@@ -0,0 +1,16 @@
+export const SuccessMessages = {
+  TEST_SUCCESS_MESSAGE: 'Operation Succeded successfully ',
+  TABLE_CREATION_SUCCESS: 'Table has been created successfully',
+};
+
+export const ErrorMessages = {
+  FORBIDDEN_ERROR: 'Forbidden / Unauthorized',
+  INTERNAL_SERVER_ERROR: 'Internal Server Error',
+  NOT_FOUND: 'Not Found',
+  BAD_REQUEST: 'Bad Request',
+  UNAUTHORIZED: 'Unauthorized',
+};
+
+export const CommonPath = {
+  TEST: '/test',
+};

2b8899b3c691df0c57405c6a6ce8b558bb1bc7dc
Manisha Jadhav <manishajadhav2323@gmail.com>
2026-06-10 20:01:25 +0530

    Added routes create and drop in seeding controller

--- src/seeding/seeding.controller.ts
+++ src/seeding/seeding.controller.ts
@@ -1,9 +1,38 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, HttpStatus, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SeedingService } from './seeding.service';
+import { SeedingModule, ErrorMessages } from 'src/common/constants/messages';
 
 @ApiTags('Seeding API: Create, seed')
 @Controller('seeding')
 export class SeedingController {
   constructor(private readonly seedingService: SeedingService) {}
+
+  @ApiOperation({ summary: SeedingModule.SUMMARY.TABLE_CREATION })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: SeedingModule.SUCCESS_MESSAGES.TABLE_CREATION_SUCCESS,
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: ErrorMessages.INTERNAL_SERVER_ERROR,
+  })
+  @Post('create')
+  createDatabase() {
+    return this.seedingService.createDatabase();
+  }
+
+  @ApiOperation({ summary: SeedingModule.SUMMARY.TABLE_DROP })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: SeedingModule.SUCCESS_MESSAGES.TABLE_DROP_SUCCESS,
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: ErrorMessages.INTERNAL_SERVER_ERROR,
+  })
+  @Post('drop')
+  dropDatabase() {
+    return this.seedingService.dropDatabase();
+  }
 }

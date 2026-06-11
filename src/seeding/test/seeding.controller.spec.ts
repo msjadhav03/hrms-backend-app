5fce285e6ba0464302508fbbe855e07384886752
Manisha Jadhav <manishajadhav2323@gmail.com>
2026-06-11 13:33:12 +0530

    Added unit test cases dor seeding.controller.ts

--- src/seeding/test/seeding.controller.spec.ts
+++ src/seeding/test/seeding.controller.spec.ts
@@ -2,22 +2,41 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { SeedingController } from '../seeding.controller';
 import { SeedingService } from '../seeding.service';
 import { HttpStatus } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 describe('SeedingController', () => {
   let controller: SeedingController;
+  let mockConfigService: Partial<ConfigService>;
   let service: SeedingService;
   const mockSeedingService = {
     createDatabase: jest.fn(),
     dropDatabase: jest.fn(),
+    seedTable: jest.fn(),
   };
 
   beforeEach(async () => {
+    mockConfigService = {
+      get: jest.fn((key: string) => {
+        const config = {
+          POSTGRES_HOST: 'localhost',
+          POSTGRES_PORT: '5432',
+          POSTGRES_USER: 'test_user',
+          POSTGRES_PASSWORD: 'test_password',
+          POSTGRES_DB: 'test_db',
+        };
+        return config[key];
+      }),
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SeedingController],
       providers: [
         {
           provide: SeedingService,
           useValue: mockSeedingService,
+        },
+        {
+          provide: ConfigService,
+          useValue: mockConfigService,
         },
       ],
     }).compile();
@@ -68,26 +87,6 @@ describe('SeedingController', () => {
 
     it('should propagate errors thrown by the service', async () => {
       const mockError = new Error('Failed to drop tables due to constraints');
-      mockSeedingService.dropDatabase.mockRejectedValue(mockError);
-      await expect(controller.dropDatabase()).rejects.toThrow(mockError);
-    });
-  });
-
-  describe('Seeding Table', () => {
-    it('should call seedingService.seedTable and return its result', async () => {
-      const mockResult = {
-        status: HttpStatus.OK,
-        message: 'Table seeding success',
-      };
-      mockSeedingService.dropDatabase.mockResolvedValue(mockResult);
-      const result = await controller.dropDatabase();
-
-      expect(service.dropDatabase).toHaveBeenCalledTimes(1);
-      expect(result).toEqual(mockResult);
-    });
-
-    it('should propagate errors thrown by the service', async () => {
-      const mockError = new Error('Failed to seed tables due to constraints');
       mockSeedingService.dropDatabase.mockRejectedValue(mockError);
       await expect(controller.dropDatabase()).rejects.toThrow(mockError);
     });

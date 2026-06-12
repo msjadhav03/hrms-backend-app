169ab215b5383b96eb168e2ef9afba23469ed1d6
Manisha Jadhav <manishajadhav2323@gmail.com>
2026-06-12 09:21:02 +0530

    [Resource:NOTIFICATION]- Added sendEmail function - Notification Service

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+import { HttpStatus, Injectable, Logger } from '@nestjs/common';
+import path from 'path';
+import { NotificationModuleConstants } from '../common/constants/messages';
 
 @Injectable()
-export class NotificationService {}
+export class NotificationService {
+  private readonly logger = new Logger(NotificationService.name);
+
+  constructor(private readonly mailerService: MailerService) {}
+
+  async sendEmail(body): Promise<any> {
+    try {
+      await this.mailerService.sendMail({
+        to: body.email,
+        subject: 'Welcome to Our Organization! 🎉',
+        template: path.resolve(__dirname, './templates/welcome'),
+        context: {
+          name: body.name,
+          email: body.email,
+          password: body.password,
+        },
+      });
+      this.logger.log(`Welcome email successfully sent to ${body.email}`);
+      return {
+        status: HttpStatus.OK,
+        message: NotificationModuleConstants.SUCCESS_MESSAGES.EMAIL_SUCCESS,
+      };
+    } catch (error) {
+      this.logger.error(
+        `Failed to send welcome email to ${body.email}: ${error.message}`,
+      );
+    }
+  }
+}

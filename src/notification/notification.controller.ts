ce6f2681c739a63136371e35293b538346c79793
Manisha Jadhav <manishajadhav2323@gmail.com>
2026-06-12 09:22:29 +0530

    [Resource:NOTIFICATION]- Added sendEmail route- Notification Controller

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
+import {
+  ApiOperation,
+  ApiProperty,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { NotificationModuleConstants } from '../common/constants/messages';
+import { NotificationService } from './notification.service';
+import { SendEmailDto } from './dto/send.email.dto';
 
 @ApiTags(NotificationModuleConstants.TAG)
 @Controller('notification')
-export class NotificationController {}
+export class NotificationController {
+  constructor(private readonly notificationService: NotificationService) {}
+
+  @ApiOperation({
+    summary: NotificationModuleConstants.SUMMARY.SEND_NOTIFICATION,
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: NotificationModuleConstants.SUCCESS_MESSAGES.EMAIL_SUCCESS,
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: NotificationModuleConstants.ERROR_MESSAGES.EMAIL_FAILED,
+  })
+  @Post('send-notification')
+  @ApiProperty()
+  sendNotification(@Body() body: SendEmailDto) {
+    return this.notificationService.sendEmail(body);
+  }
+}

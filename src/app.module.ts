2ad1df17c667da0fed3d27c6084269d1f06a2825
Manisha Jadhav <manishajadhav2323@gmail.com>
2026-06-13 14:22:15 +0530

    [Resource:AUTH]- Registered Auth Module in App Module - Auth Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,29 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { HttpExceptionFilter } from './common/filters/http-expection.filter';
 import { DatabasePostgresModule } from './database/database.module';
 import { SeeedingModule } from './seeding/seeding.module';
 import { NotificationModule } from './notification/notification.module';
 import { EmployeeModule } from './employee/employee.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    AuthModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '1h',
+        },
+      }),
     }),
     DatabasePostgresModule,
     SeeedingModule,

b26e6926046b7350af855447476c505ec9013968
Manisha Jadhav <manishajadhav2323@gmail.com>
2026-06-13 16:08:28 +0530

    [Resource:DASHBOARD] - Updated imports in Dashboard Module - Added JWT module

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -3,9 +3,10 @@ import { DashboardController } from './dashboard.controller';
 import { DashboardService } from './dashboard.service';
 import { DatabaseProviders } from '../database/database.service';
 import { AuthModule } from '../auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, JwtModule],
   controllers: [DashboardController],
   providers: [...DatabaseProviders, DashboardService],
 })

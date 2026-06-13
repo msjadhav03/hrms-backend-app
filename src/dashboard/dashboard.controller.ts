6233f85ee91841a4695da0d82254faab1db6c7ff
Manisha Jadhav <manishajadhav2323@gmail.com>
2026-06-13 16:14:48 +0530

    [Resource:Dashboard] - Applied Role Guard to Dashboard Routes

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -2,9 +2,10 @@ import { Controller, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { DashboardModuleConstants } from '../common/constants/messages';
 import { AuthGuard } from '../auth/auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
 
 @ApiTags(DashboardModuleConstants.TAG)
 @ApiBearerAuth('JWT-auth')
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard, RolesGuard)
 @Controller('dashboard')
 export class DashboardController {}

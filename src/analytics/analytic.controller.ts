105893361b07760efb725475043ad0b10be1ff85
Manisha Jadhav <manishajadhav2323@gmail.com>
2026-06-13 15:15:00 +0530

    [Resource:ANALYTIC]- Applied Auth Guard and Imported Auth Module

--- src/analytics/analytic.controller.ts
+++ src/analytics/analytic.controller.ts
@@ -1,7 +1,10 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AnalyticModuleConstants } from '../common/constants/messages';
+import { AuthGuard } from '../auth/auth.guard';
 
 @ApiTags(AnalyticModuleConstants.TAG)
+@ApiBearerAuth('JWT-auth')
+@UseGuards(AuthGuard)
 @Controller('analytic')
 export class AnalyticController {}
--- src/analytics/analytic.module.ts
+++ src/analytics/analytic.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AnalyticController } from './analytic.controller';
 import { AnalyticService } from './analytic.service';
 import { DatabaseProviders } from '../database/database.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AnalyticController],
   providers: [...DatabaseProviders, AnalyticService],
 })

01c0e02aea690b6179d21eac881eab9b6584d31f
Manisha Jadhav <manishajadhav2323@gmail.com>
2026-06-13 15:15:22 +0530

    [Resource:Dashboard]- Added Mock for JWT service and Config Service - Unit Testcase

--- src/dashboard/test/dashboard.controller.spec.ts
+++ src/dashboard/test/dashboard.controller.spec.ts
@@ -1,14 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DashboardService } from '../dashboard.service';
 import { DashboardController } from '../dashboard.controller';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 describe('Dashboard Controller', () => {
   let dashboardService: DashboardService;
   let dashboardController: DashboardController;
+  let jwtService: any;
+  let configService: any;
+  const mockConfigValues: Record<string, any> = {
+    JWT_SECRET: 'fsdfnsfdn',
+  };
 
   beforeEach(async () => {
+    jwtService = {
+      signAsync: jest.fn().mockResolvedValue('sdfsdfsdf'),
+      logger: jest.fn(),
+    };
+    configService = {
+      get: jest.fn().mockResolvedValue(mockConfigValues),
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DashboardController],
-      providers: [DashboardService],
+      providers: [
+        DashboardService,
+        {
+          provide: JwtService,
+          useValue: jwtService,
+        },
+        {
+          provide: ConfigService,
+          useValue: configService,
+        },
+      ],
     }).compile();
 
     dashboardService = module.get<DashboardService>(DashboardService);

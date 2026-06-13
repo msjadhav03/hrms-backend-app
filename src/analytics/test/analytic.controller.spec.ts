9b9b9037cc7302e07e53ed2c26528dde34bd2eaf
Manisha Jadhav <manishajadhav2323@gmail.com>
2026-06-13 15:14:18 +0530

    [Resource:ANALYTIC]- Added Mock for JWT service and Config Service - Unit Testcase

--- src/analytics/test/analytic.controller.spec.ts
+++ src/analytics/test/analytic.controller.spec.ts
@@ -1,15 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AnalyticController } from '../analytic.controller';
 import { AnalyticService } from '../analytic.service';
-import { after } from 'node:test';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 describe('Analytic Controller', () => {
   let analyticService: AnalyticService;
   let analyticController: AnalyticController;
+  let jwtService: any;
+  let configService: any;
+  const mockConfigValues: Record<string, any> = {
+    JWT_SECRET: 'fsdfnsfdn',
+  };
   beforeEach(async () => {
+    jwtService = {
+      signAsync: jest.fn().mockResolvedValue('sdfsdfsdf'),
+      logger: jest.fn(),
+    };
+    configService = {
+      get: jest.fn().mockResolvedValue(mockConfigValues),
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AnalyticController],
-      providers: [AnalyticService],
+      providers: [
+        AnalyticService,
+        {
+          provide: JwtService,
+          useValue: jwtService,
+        },
+        {
+          provide: ConfigService,
+          useValue: configService,
+        },
+      ],
     }).compile();
     analyticController = module.get<AnalyticController>(AnalyticController);
     analyticService = module.get<AnalyticService>(AnalyticService);

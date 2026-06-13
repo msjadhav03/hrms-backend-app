d1a3304348c2d6ca6dacbe7af1b5a8ce2566d1c9
Manisha Jadhav <manishajadhav2323@gmail.com>
2026-06-13 18:07:09 +0530

    [Resource:Dashboard] - Added Provider and route for salary-trend

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,11 +1,47 @@
-import { Controller, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { DashboardModuleConstants } from '../common/constants/messages';
+import { Controller, Get, HttpStatus, Query, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import {
+  DashboardModuleConstants,
+  ErrorMessages,
+} from '../common/constants/messages';
 import { AuthGuard } from '../auth/auth.guard';
 import { RolesGuard } from '../auth/roles.guard';
+import { FilterOptionDto } from './dto/filter.dto';
+import { DashboardService } from './dashboard.service';
 
 @ApiTags(DashboardModuleConstants.TAG)
 @ApiBearerAuth('JWT-auth')
 @UseGuards(AuthGuard, RolesGuard)
 @Controller('dashboard')
-export class DashboardController {}
+export class DashboardController {
+  constructor(private readonly dashboardService: DashboardService) {}
+  /**
+   * @param filter - Object Containing country and/or department values
+   * @returns - Object containing success response with MIN, MAX and Avg values
+   */
+  @ApiOperation({
+    summary: DashboardModuleConstants.SUMMARY.SUMMARY_DASHBOARD_VARIATION_TREND,
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description:
+      DashboardModuleConstants.SUCCESS_MESSAGES.SUCCESS_DASHBOARD_MIN_MAX_AVG,
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: ErrorMessages.INTERNAL_SERVER_ERROR,
+  })
+  @ApiResponse({
+    status: HttpStatus.FORBIDDEN,
+    description: ErrorMessages.FORBIDDEN_ERROR,
+  })
+  @Get('/salary-trend')
+  async findSalaryAvgMinMax(@Query() filter: FilterOptionDto) {
+    return this.dashboardService.getSalaryTrend(filter);
+  }
+}
--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -1,4 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpStatus,
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { Pool } from 'pg';
+import { DashboardModuleConstants } from '../common/constants/messages';
+import { FilterOptionDto } from './dto/filter.dto';
 
 @Injectable()
-export class DashboardService {}
+export class DashboardService {
+  constructor(@Inject('PG_CONNECTION') private readonly db: Pool) {}
+  private readonly logger = new Logger(DashboardService.name);
+  /**
+   * @param filter - Optional filter to narrow down the salary trend by country and/or department
+   * @returns - Dynamically built where clause based on the applied filters to be used in the salary trend query
+   */
+  buildQuery(filter?: FilterOptionDto) {
+    try {
+      let whereClause = '';
+      if (filter?.department) {
+        whereClause = whereClause + ` AND department = '${filter.department}'`;
+      }
+      if (filter?.country) {
+        whereClause = whereClause + `AND country = '${filter.country}'`;
+      }
+      return whereClause;
+    } catch (error) {
+      this.logger.error(error.message);
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+
+  /**
+   * @param filter - Optional filter to narrow down the salary trend by country and/or department
+   * @returns Object containing the minimum, maximum and average salary based on the applied filters
+   */
+  async getSalaryTrend(filter?: FilterOptionDto) {
+    try {
+      let query = `Select min(salary), max(salary), avg(salary) from employees`;
+      if (filter?.country != null || filter?.department != null) {
+        let whereClause = this.buildQuery(filter);
+        query = `${query} WHERE 1=1 ${whereClause}`;
+      }
+
+      console.log(query);
+      const response = await this.db.query(query);
+      return {
+        status: HttpStatus.OK,
+        message:
+          DashboardModuleConstants.SUCCESS_MESSAGES
+            .SUCCESS_DASHBOARD_MIN_MAX_AVG,
+        data: response?.rows,
+      };
+    } catch (error) {
+      this.logger.error(error.message);
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+}

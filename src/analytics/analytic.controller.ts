bc5d6349588ff3b235c3cae70297e94818fb9df8
Manisha Jadhav <manishajadhav2323@gmail.com>
2026-06-13 16:15:40 +0530

    [Resource:ANALYTIC] - Applied Role Guard to Analytic Routes

--- src/analytics/analytic.controller.ts
+++ src/analytics/analytic.controller.ts
@@ -2,9 +2,10 @@ import { Controller, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AnalyticModuleConstants } from '../common/constants/messages';
 import { AuthGuard } from '../auth/auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
 
 @ApiTags(AnalyticModuleConstants.TAG)
 @ApiBearerAuth('JWT-auth')
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard, RolesGuard)
 @Controller('analytic')
 export class AnalyticController {}
